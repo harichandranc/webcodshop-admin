61bdfff61e6c0adfadedcbf96da715448df08e2a
Harichandran C <harichstech@gmail.com>
2026-06-17 03:03:49 +0530

    Added form page

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -3,27 +3,26 @@ import Topbar from "../components/Topbar";
 
 function AdminLayout({ children }) {
   return (
-    <div
-      style={{
-        display: "flex",
-        minHeight: "100vh",
-        background: "#f5f7fb",
-      }}
-    >
+    <div>
+      {/* SIDEBAR (fixed) */}
       <Sidebar />
 
+      {/* MAIN AREA */}
       <div
         style={{
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
+          marginLeft: "250px", // IMPORTANT (matches sidebar width)
+          minHeight: "100vh",
+          background: "#0b1220",
         }}
       >
+        {/* TOPBAR */}
         <Topbar />
 
+        {/* PAGE CONTENT */}
         <div
           style={{
             padding: "25px",
+            paddingTop: "95px", // IMPORTANT (topbar height + spacing)
           }}
         >
           {children}
